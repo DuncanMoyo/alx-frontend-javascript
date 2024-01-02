f739b1cb7e74a0cd7fb899bbeb76ee7f8794cf04
DuncanMoyo <duncanfmoyo@gmail.com>
2024-01-02 15:03:48 +0200

    Task 14: Iterate through object

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,14 @@
+const iterateThroughObject = (reportWithIterator) => {
+  let result = "";
+  let nextEmployee = reportWithIterator.next();
+  while (!nextEmployee.done) {
+    result += nextEmployee.value;
+    nextEmployee = reportWithIterator.next();
+    if (!nextEmployee.done) {
+      result += " | ";
+    }
+  }
+  return result;
+};
+
+export default iterateThroughObject;
