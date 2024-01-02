43f69c9a878815657d9ca1554d8a2ab1612bc090
DuncanMoyo <duncanfmoyo@gmail.com>
2024-01-02 14:03:42 +0200

    Task 1 - Block Scope

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,13 @@
+const taskBlock = (trueOrFalse) => {
+  var task = false;
+  var task2 = true;
+
+  if (trueOrFalse) {
+    let task = true;
+    let task2 = false;
+  }
+
+  return [task, task2];
+};
+
+export default taskBlock;
