e50a49ac56e03f8d346a5a34c87814ba156224eb
DuncanMoyo <duncanfmoyo@gmail.com>
2024-01-03 14:05:30 +0200

    refactored problematic functions

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,21 +1,7 @@
-const createIteratorObject = (report) => {
-  let employees = [];
-  for (const department in report.allEmployees) {
-    if (Object.prototype.hasOwnProperty.call(report.allEmployees, department)) {
-      employees = employees.concat(report.allEmployees[department]);
-    }
+export default function createIteratorObject(report) {
+  let allEmployees = [];
+  for (const item of Object.values(report.allEmployees)) {
+    allEmployees = [...allEmployees, ...item];
   }
-  let index = 0;
-  return {
-    next() {
-      if (index < employees.length) {
-        const value = employees[index];
-        index += 1;
-        return { value, done: false };
-      }
-      return { done: true };
-    },
-  };
-};
-
-export default createIteratorObject;
+  return allEmployees;
+}
