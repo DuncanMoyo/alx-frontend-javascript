b61d53972ba6de2664e914fbed3c6906c433e5e7
DuncanMoyo <duncanfmoyo@gmail.com>
2024-01-08 23:23:21 +0200

    Added a summation

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,10 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  const count = weakMap.get(endpoint) || 0;
+  let count = weakMap.get(endpoint) || 0;
+  count += 1;
   if (count >= 5) {
-    throw new Error('Endpoint load is high');
+    throw new Error("Endpoint load is high");
   }
-  weakMap.set(endpoint, count + 1);
+  weakMap.set(endpoint, count);
 }
